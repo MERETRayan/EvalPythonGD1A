3680c985bd4f3682d69e46b6d2e6551709a55d38
MERETRayan <74654820+MERETRayan@users.noreply.github.com>
2020-12-09 10:41:39 +0100

    Update Programme.py

--- Programme.py
+++ Programme.py
@@ -2,6 +2,7 @@ from colorama import Fore, Back, Style
 
 def chercheLettreCorrect(motEcrit,motDevine) :
     i = 0 
+    j = 0
     for j in range (0,5):  
         for i in range (0,5):
             if motDevine[j]==motEcrit[i]:
@@ -9,15 +10,21 @@ def chercheLettreCorrect(motEcrit,motDevine) :
 
 def chercheLettreFause(motEcrit,motDevine) :
     i = 0 
+    j = 0
     for j in range (0,5):  
         for i in range (0,5):
-            if motDevine[i]==motEcrit[j]:
+            if motDevine[i]!=motEcrit[j]:
                print (Back.BLUE+motEcrit[j])
 
 def chercheLettreMalPlacé(motEcrit,motDevine) :
     i = 0 
+    j = 0
     for j in range (0,5):  
         for i in range (0,5):
             if motDevine[j]==motEcrit[i]:
-               print (Back.RED+motEcrit[i])
+               print (Back.YELLOW+motEcrit[i])
 
+motADeviner=['a','r','b','r','e','s']
+motEcris=input("Quel est le mot caché en 6 lettres ?")
+print ( motEcris)
+print (motADeviner)
